f01094ac148bae9c79c7752eb602eb2ac394db0d
Neven <58472164+neven14@users.noreply.github.com>
2020-12-14 07:59:20 +0100

    Add files via upload

---
+++ RichBox/RichBox/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RichBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SaveFile(@"C:Tempproba.rtf");
+            MessageBox.Show("Tekst spremljen");
+        }
+
+        private void btnCitaj_Click(object sender, EventArgs e)
+        {
+            richTextBox1.LoadFile(@"C:Tempproba.rtf");
+        }
+
+        private void btnBrisi_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
